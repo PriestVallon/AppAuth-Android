4d755dcf709a3574049d92906cb4e148364352c3
Eoin Stack <estack@ecanvasser.com>
2022-02-15 12:25:16 +0000

    Wrong comparison

--- library/java/net/openid/appauth/AuthorizationService.java
+++ library/java/net/openid/appauth/AuthorizationService.java
@@ -694,7 +694,7 @@ public class AuthorizationService {
                 return;
             }
 
-            if (response.idToken != null && response.accessToken != null && response.accessToken != response.idToken) {
+            if (response.idToken != null && response.accessToken != null && !response.accessToken.equalsIgnoreCase(response.idToken)) {
                 IdToken idToken;
                 try {
                     idToken = IdToken.from(response.idToken);
